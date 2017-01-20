d5405a05f2209ba29b4b74bc88307ade90aba4ed
valdirachman <valdi@yesbossnow.com>
2017-01-20 09:00:31 +0700

    Finish email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      'valrchmn@gmail.com',
-    password:       'lateralthinking123!',
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
     domain:         'localhost:3000',
     address:       'smtp.gmail.com',
     port:          '587',
