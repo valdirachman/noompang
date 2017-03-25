48b7ae7c9955f0b64a1caf17a56a7c1dcd599f4e
mirsadikin <moh.emirsaid@hotmail.com>
2017-03-25 22:37:30 +0700

    All landing page are linked and functional

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,16 @@ Rails.application.routes.draw do
 
   get '/home' => 'pages#home', as: 'home'
 
+  get 'users/sign_in' => 'users/sign_in'
+
+  get 'how-it-works' => 'pages#how-it-works'
+
+  get 'stories' => 'pages#stories'
+
+  get 'FAQ' => 'pages#FAQ'
+
+  get 'about-us' => 'pages#about-us'
+
   get '/user/:id' => 'pages#profile', as: 'user_profile'
 
   get '/explore' => 'pages#explore'
