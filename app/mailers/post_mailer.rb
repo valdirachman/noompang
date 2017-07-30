efd8f6269824969fbfc222738c5cae1e6956fbbe
Mirsa Sadikin <moh.emirsaid@hotmail.com>
2017-07-31 00:15:01 +0700

    polish UI v1.5

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -3,6 +3,6 @@ class PostMailer < ApplicationMailer
   def post_email(user, post)
     @user = user
     @post = post
-    mail(to: @user.email, subject: "Join " + @user.username + "'s ride!")
+    mail(to: @user.email, subject: @user.username + " posted a new ride. Join the ride now!")
   end
 end
