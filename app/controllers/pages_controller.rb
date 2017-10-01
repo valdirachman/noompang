27bc750a1d66e6bbb211b2f5d17536e6da2f72aa
Mirsa Sadikin <moh.emirsaid@hotmail.com>
2017-10-01 13:09:47 +0700

    edit email

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -105,14 +105,14 @@ class PagesController < ApplicationController
     if (not friends.empty?)
       reference_friend = friends.find(friends.pluck(:id).shuffle.first)
       friends_of_reference_friend = reference_friend.friends.find_not_friends(current_user)
-      @recommended_friends = User.where(id: friends_of_reference_friend.map(&:id).sample(10)) - [current_user]
+      @recommended_friends = User.where(id: friends_of_reference_friend.map(&:id).sample(50)) - [current_user]
       if @recommended_friends.empty?
         new_friends = User.find_not_friends(current_user)
-        @recommended_friends = User.where(id: new_friends.map(&:id).sample(10)) - [current_user]
+        @recommended_friends = User.where(id: new_friends.map(&:id).sample(50)) - [current_user]
       end
     else
       new_friends = User.find_not_friends(current_user)
-      @recommended_friends = User.where(id: new_friends.map(&:id).sample(10)) - [current_user]
+      @recommended_friends = User.where(id: new_friends.map(&:id).sample(50)) - [current_user]
     end
   end
 
